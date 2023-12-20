76a5b0d207a0caa7bb3d5887c0c6bbc04c3c95ee
mx <>
2023-12-20 06:36:04 +0000

    Saving progress.

--- scripts/login_posh.py
+++ scripts/login_posh.py
@@ -9,26 +9,26 @@ from modules.get_from_viatuix_config import get_from_viatuix_config
 os.environ['DISPLAY'] = ':1'
 
 if __name__ == '__main__':
-  # Make a new browser tab
-  pyautogui.hotkey('ctrl', 't')
-  time.sleep(3)
-  screenshot = create_screenshot_with_grid(100)
-  screenshot.save('chromium-nix-screenshots/posh-1.png')
+  ## Make a new browser tab
+  #pyautogui.hotkey('ctrl', 't')
+  #time.sleep(3)
+  #screenshot = create_screenshot_with_grid(100)
+  #screenshot.save('chromium-nix-screenshots/posh-1.png')
 
-  # Type in github url into bar
-  login_page = get_from_viatuix_config('viatuix.json', 'url2')
-  pyautogui.moveTo(200, 75)
-  pyautogui.click()
-  pyautogui.typewrite(login_page)
-  time.sleep(2)
-  pyautogui.press('enter')
-  time.sleep(5)
-  screenshot = create_screenshot_with_grid(100)
-  screenshot.save('chromium-nix-screenshots/posh-2.png')
+  ## Type in github url into bar
+  #login_page = get_from_viatuix_config('viatuix.json', 'url2')
+  #pyautogui.moveTo(200, 75)
+  #pyautogui.click()
+  #pyautogui.typewrite(login_page)
+  #time.sleep(2)
+  #pyautogui.press('enter')
+  #time.sleep(5)
+  #screenshot = create_screenshot_with_grid(100)
+  #screenshot.save('chromium-nix-screenshots/posh-2.png')
 
   ## Type in username
   #username = get_from_viatuix_config('viatuix.json', 'username')
-  #pyautogui.moveTo(414, 353)
+  #pyautogui.moveTo(300, 550)
   #pyautogui.click()
   #pyautogui.typewrite(username)
   #time.sleep(5)
@@ -37,7 +37,7 @@ if __name__ == '__main__':
 
   ## Type in password
   #password = get_from_viatuix_config('viatuix.json', 'password')
-  #pyautogui.moveTo(423, 425)
+  #pyautogui.moveTo(300, 600)
   #pyautogui.click()
   #pyautogui.typewrite(password)
   #time.sleep(5)
@@ -45,15 +45,20 @@ if __name__ == '__main__':
   #screenshot.save('chromium-nix-screenshots/posh-4.png')
 
   ## Submit username and password
-  #pyautogui.moveTo(502, 477)
-  #pyautogui.click()
+  #pyautogui.press('enter')
   #time.sleep(5)
   #screenshot = create_screenshot_with_grid(100)
   #screenshot.save('chromium-nix-screenshots/posh-5.png')
 
   ## Don't save password
-  #pyautogui.moveTo(700, 400)
+  #pyautogui.press('esc')
   #pyautogui.click()
   #time.sleep(5)
   #screenshot = create_screenshot_with_grid(100)
   #screenshot.save('chromium-nix-screenshots/posh-6.png')
+
+  pyautogui.moveTo(700, 400)
+  pyautogui.click()
+  time.sleep(5)
+  screenshot = create_screenshot_with_grid(100)
+  screenshot.save('chromium-nix-screenshots/posh-7.png')
