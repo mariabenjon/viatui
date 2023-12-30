5a846fae73a6ba3082d4a5d396e80cce2f35d50f
mx <>
2023-12-29 21:41:17 -0700

    Saving progress: zoom.

--- modules/zoom_image.py
+++ modules/zoom_image.py
@@ -12,12 +12,16 @@ def zoom_image(image_path, output_path, top_left, bottom_right):
     try:
         # Open an image file
         with Image.open(image_path) as img:
+            original_size = img.size
             # Crop the image to the specified region
             cropped_img = img.crop((*top_left, *bottom_right))
 
+            # Resize cropped image to original dimensions
+            resized_img = cropped_img.resize(original_size, Image.ANTIALIAS)
+
             # Save the cropped image
-            cropped_img.save(output_path)
-            print(f"Zoomed image saved to {output_path}")
+            resized_img.save(output_path)
+            print(f"Zoomed and resized image saved to {output_path}")
 
     except Exception as e:
         print(f"An error occurred: {e}")
